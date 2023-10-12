a217dda6eb28e109a0a2118712822c3a6bf7dbcd
glebanuk <glebbong12387@gmail.com>
2023-10-12 18:47:42 +0300

    feat:add solution to 'reverse int' task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+function reverse(n) {
+    let str = n.toString().split("").reverse().join("");
+
+    return parseInt(str, 10);
 }
+
+module.exports = reverse;
+
+
